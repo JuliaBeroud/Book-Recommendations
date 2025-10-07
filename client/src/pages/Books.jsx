c613272e129c3f83f749134f3801c0c164a0f5fe
Julia Beroud <juliaberoud@gmail.com>
2025-10-07 15:23:23 -0700

    implement list page with cover renderings

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,52 +1,72 @@
- import React from 'react'
- import { useEffect } from 'react'
- import { useState } from 'react'
- import axios from 'axios'
- import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
- const Books = () => {
-    
-    const [books,setBooks] = useState([])
+const API_BASE = "http://localhost:8800";
+const toCoverSrc = (cover) =>
+  !cover ? "" : cover.startsWith("http") ? cover : `${API_BASE}${cover}`;
 
-    useEffect(()=>{
-        const fetchAllBooks = async()=>{
-            try{
-                const res = await axios.get("http://localhost:8800/books")
-                setBooks(res.data)
-            }catch(err){
-                console.log(err)
-            }
-        }
-        fetchAllBooks()
-    },[])
+const Books = () => {
+  const [books, setBooks] = useState([]);
 
-    const handleDelete = async (id)=>{
-        try{
-            await axios.delete("http://localhost:8800/books/"+id)
-            window.location.reload()
-        }catch(err){
-            console.log(err)
-        }
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get(`${API_BASE}/books`);
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${API_BASE}/books/${id}`);
+      setBooks((prev) => prev.filter((b) => b.id !== id));
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (
-        <div>
-            <h1>Julia's Book Recommendations</h1>
-            <div className="books">
-                {books.map(book=>(
-                    <div className="book" key={book.id}>
-                       {book.cover && <img src={book.cover} alt="" />}
-                       <h2>{book.title}</h2> 
-                       <p>{book.desc}</p> 
-                       <span>{book.price}</span>
-                       <button className="delete" onClick={()=>handleDelete(book.id)}>Delete</button>
-                       <button className="update"><Link to={`/update/${book.id}`}>Update</Link></button>
-                    </div>
-                ))}
-            </div>
-            <button className="newBook"><Link to="/add">Add a new book</Link></button>
-        </div>
-    )
- };
+  return (
+    <div>
+      <h1>Julia's Book Recommendations</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && (
+              <img
+                src={toCoverSrc(book.cover)}
+                alt={`${book.title} cover`}
+                style={{
+                  width: 160,
+                  height: 220,
+                  objectFit: "cover",
+                  borderRadius: 6,
+                  display: "block",
+                }}
+                onError={(e) => (e.currentTarget.style.display = "none")}
+              />
+            )}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+            <button className="delete" onClick={() => handleDelete(book.id)}>
+              Delete
+            </button>
+            <button className="update">
+              <Link to={`/update/${book.id}`}>Update</Link>
+            </button>
+          </div>
+        ))}
+      </div>
+      <button className="newBook">
+        <Link to="/add">Add a new book</Link>
+      </button>
+    </div>
+  );
+};
 
- export default Books
+export default Books;
